4e26706ea81738e52ec1b71240cdc558fb567286
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2026-02-14 18:19:58 +0530

    ROS-adapter interface

---
+++ source-code/adapters/ros-adapter/index.js
@@ -0,0 +1,12 @@
+/**
+ * ROS Adapter - Route Optimization System
+ * @description This module doesn't need a adapter but is implemented for future proofing
+ * 
+ * @interface ROSAdapter
+ * 
+ * @method getOptimalRoute - Get the optimized route for a given set of waypoints
+ * @param {string} origin - The starting point of the route
+ * @param {Array} waypoints - An array of waypoints to optimize the route for
+ * @returns {Promise} - A promise that resolves to the optimized route
+ *
+ */
