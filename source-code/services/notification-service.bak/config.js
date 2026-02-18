74e1da02032ab7e9befc3ccaad8933e85127edf4
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2026-02-18 20:31:57 +0530

    Fixed docker issue with services not having access to utilities

--- source-code/services/notification-service.bak/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rabbitMQ: {
-        url: process.env.RABBITMQ_URL || 'amqp://localhost',
-        exchangeName: 'order_exchange', // Same exchange as order-service
-        queueName: 'notifications_queue'
-    }
-};
--- source-code/services/order-service.bak/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rabbitMQ: {
-        url: process.env.RABBITMQ_URL || 'amqp://localhost',
-        exchangeName: 'order_exchange',
-        queueName: 'orders_queue'
-    }
-};
